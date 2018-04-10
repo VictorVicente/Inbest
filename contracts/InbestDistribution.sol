20ae45d3ca7b5c4c74e50e6f8b1e6aedbe13099e
Victor Vicente <victor.g.vicente@gmail.com>
2018-04-10 14:49:22 -0300

    Allocations allowed only before startTime

--- contracts/InbestDistribution.sol
+++ contracts/InbestDistribution.sol
@@ -94,6 +94,7 @@ contract InbestDistribution is Ownable {
     */
   function setAllocation (address _recipient, uint256 _totalAllocated) onlyOwnerOrAdmin public {
     require(_recipient != address(0));
+    require(startTime > now); //Allocations are allowed only before starTime
     require(allocations[_recipient].totalAllocated == 0 && _totalAllocated > 0); // Must be the first and only allocation for this recipient
     require(_recipient != companyWallet); // Receipient of presale allocation can't be company wallet
 
